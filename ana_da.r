44f6fb267e662d52f54e73c202bc5944f457c7ce
EC2 Default User <ec2-user@ip-172-31-32-195.us-east-2.compute.internal>
2019-02-07 10:27:36 +0000

    Rかえた

--- ana_da.r
+++ ana_da.r
@@ -1,34 +1,33 @@
+tablecsv<-function(datalm,data,ou){
+    sum<-summary(datalm)
+    coe <- sum$coefficient
+    N <- nrow(data)
+    aic <- AIC(datalm)
+    result <- cbind(coe,aic,N)
+    result[2:nrow(result),5:6] <- ""
+    
+    write.table(matrix(c("\n",colnames(result)),nrow=1),ou,append=T,quote=F,sep=","
+    ,row.names=F,col.names=F)
+    write.table(result,ou,append=T,quote=F,sep=",",row.names=T,col.names=F)
+}
+
 args <- commandArgs(trailingOnly=T)
 file1 <-args[1]
 file2 <-args[2]
 out <-args[3]
-out2<-args[4]
 buf1<-read.csv(file1, header=T)
 buf2<-read.csv(file2, header=T)
 
-sink(out,append=TRUE)
 data<-rbind(buf1,buf2)
 data1.glm<-glm(formula = death ~ dbh01 + crd1 + crd2 + crd3 + crd4 + crd5 + crd6 + crd7 + crd8 + crd9 + kabudachi,family=binomial, data = data)
-data1sum<-summary(data1.glm)
-print(data1sum)
 data2.step<-step(data1.glm)
-
-data2sum<-summary(data2.step)
-print(data2sum)
 data3.glm<-glm(formula = death ~ dbh01 + crd+ kabudachi,family=binomial, data = data)
-data3sum<-summary(data3.glm)
-print(data3sum)
 data4.step<-step(data3.glm)
 
-data4sum<-summary(data4.step)
-print(data4sum)
+tablecsv(data1.glm,data,out)
 
-coe <- data4sum$coefficient
-N <- nrow(data)
-aic <- AIC(data4.step)
-result <- cbind(coe,aic,N)
-result[2:nrow(result),5:6] <- ""
+tablecsv(data2.step,data,out)
 
-write.table(matrix(c("",colnames(result)),nrow=1),out2,append=T,quote=F,sep=","
-,row.names=F,col.names=F)
-write.table(result,out2,append=T,quote=F,sep=",",row.names=T,col.names=F)
+tablecsv(data3.glm,data,out)
+
+tablecsv(data4.step,data,out)
--- ana_grow.r
+++ ana_grow.r
@@ -1,44 +1,36 @@
+tablecsv<-function(datalm,data,ou){
+    sum<-summary(datalm)
+    coe <- sum$coefficient
+    N <- nrow(data)
+    aic <- AIC(datalm)
+    result <- cbind(coe,aic,N)
+    ro<-nrow(result)
+    if(ro>1){
+        result[2:nrow(result),5:6] <- ""
+    }
+    
+    write.table(matrix(c("\n",colnames(result)),nrow=1),ou,append=T,quote=F,sep=","
+    ,row.names=F,col.names=F)
+    write.table(result,ou,append=T,quote=F,sep=",",row.names=T,col.names=F)
+}
+
 args <- commandArgs(trailingOnly=T)
 file1 <-args[1]
 file2 <-args[2]
-
-out1 <-args[3]
-out2<-args[4]
+out <-args[3]
 buf1<-read.csv(file1, header=T)
 buf2<-read.csv(file2, header=T)
 
-database<-rbind(buf1,buf2)
+data<-rbind(buf1,buf2)
+data1.glm<-lm(formula = growth ~ dbh01 + crd1 + crd2 + crd3 + crd4 + crd5 + crd6 + crd7 + crd8 + crd9 + kabudachi, data = data)
+data2.step<-step(data1.glm)
+data3.glm<-lm(formula = growth ~ dbh01 + crd+ kabudachi, data = data)
+data4.step<-step(data3.glm)
 
-#sink(out1,append=TRUE)
+tablecsv(data1.glm,data,out)
 
-data1.lm<-lm(formula = growth ~ dbh01 + crd1 + crd2 + crd3 + crd4 + crd5 + crd6 + crd7 + crd8 + crd9 + kabudachi, data = database)
+tablecsv(data2.step,data,out)
 
+tablecsv(data3.glm,data,out)
 
-data1sum<-summary(data1.lm)
-print(data1sum)
-data2.step<-step(data1.lm)
-
-data2sum<-summary(data2.step)
-print(data2sum)
-data3.lm<-lm(formula = growth ~ dbh01 + crd+ kabudachi, data = database)
-data3sum<-summary(data3.lm)
-print(data3sum)
-data4.step<-step(data3.lm)
-
-data4sum<-summary(data4.step)
-print(data4sum)
-
-#column<-c("growth","dbh01","crd","kabudachi")
-#data5<-data[,column]
-#library(ggplot2)
-
-coe <- data4sum$coefficient
-N <- nrow(database)
-aic <- AIC(data4.step)
-result <- cbind(coe,aic,N)
-print("439")
-result[2:nrow(result),5:6] <- ""
-
-write.table(matrix(c("",colnames(result)),nrow=1),out2,append=T,quote=F,sep=","
-,row.names=F,col.names=F)
-write.table(result,out2,append=T,quote=F,sep=",",row.names=T,col.names=F)
+tablecsv(data4.step,data,out)
